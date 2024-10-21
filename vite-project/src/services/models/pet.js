6ccfb939d46aa1073a78594000e082752f5dfcef
Gianna DAconti <themeanmememan@gmail.com>
2024-10-21 19:52:03 -0400

    updated code

---
+++ vite-project/src/services/models/pet.js
@@ -0,0 +1,14 @@
+export interface Pet {
+    id: number;
+    name : string;
+    animalType: 'dog';
+    age: Number,
+    description: string;
+    image: string;
+    breed: string;
+    isBoy: boolean;
+    isAdopted: boolean; 
+
+
+
+}
