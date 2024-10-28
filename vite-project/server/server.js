a74445fb0ab88245482852fa4f883bf6c6221495
yunusemreertas <yunus29emre@outlook.com>
2024-10-28 19:40:50 -0400

    backend part updated

--- vite-project/server/server.js
+++ vite-project/server/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import sequelize from './config/database.js';
 import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';  // Import the auth routes
+import authRoutes from './routes/authRoutes.js';
 
-dotenv.config();  // Load environment variables
+dotenv.config();
 
 const app = express();
 
@@ -12,10 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Register the auth routes
-app.use('/api/auth', authRoutes);  // Register the routes under /api/auth
+// Register auth routes
+app.use('/api/auth', authRoutes);
 
-// Sync Sequelize models and start the server
 sequelize.sync().then(() => {
   const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
